0f9fb5d346e22afbf0b0d5d7a828dfd9ce00e48b
Ruddy MORENO <contactantilles@gmail.com>
2023-07-20 14:32:02 +0200

    test commit

---
+++ 1.c
@@ -0,0 +1,16 @@
+/* #include --- Inclusion de fonctions standard ou locales */
+#include <stdio.h>
+#include <stdlib.h>
+/* Envoie Hello world! sur la sortie standard */
+// Commentaire en C99 (interdit en C89)
+
+int main(int argc, char* argv[]) {
+
+    // Fonction native en C
+    puts("Hello world 5!");
+
+    // Valeur de retour de la fonction main
+    return EXIT_SUCCESS;
+
+}
+
